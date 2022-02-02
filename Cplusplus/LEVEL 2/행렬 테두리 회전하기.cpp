ae205377a3a12b1bb96eba59a59f7a0c37b40bbd
CJHofficial <ckwnsgur9690@naver.com>
2022-02-02 23:51:25 +0900

    0202 행렬 테두리 회전하기(미완)

---
+++ Cplusplus/LEVEL 2/행렬 테두리 회전하기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int arr[102][102] = { 0, };
+
+vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
+    vector<int> answer;
+    for (int i = 1; i < rows; i++) {
+        for (int j = 1; j < columns; j++) {
+            queries[i][j] = j + 6 * (i - 1);
+        }
+    }
+    int startRow, startCol;
+    int finalRow, finalCol;
+    for (auto v : queries) {
+        nowRow = v[0];
+        nowCol = v[1];
+        for (int i = nowCol; i < )
+    }
+    return answer;
+}
