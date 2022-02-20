adbdeb9c1579231c29a8585da77abc1fdd469095
beomseok <bmtosss@gmail.com>
2022-02-20 12:36:38 +0900

    수정

--- Python/level2/큰 수 만들기.py
+++ Python/level2/큰 수 만들기.py
@@ -6,3 +6,17 @@ def solution(number, k):
             k -= 1
         stack.append(n)
     return ''.join(stack[:len(number) - k])
+
+
+def solution(number, k):
+    return greedy(number, k, 0, '')
+
+def greedy(number, k, index, answer):
+    if k == 0:
+        answer += number[index:]
+        return answer
+    if len(number) - index == k:
+        return answer
+    remove = greedy(number, k - 1, index + 1, answer)
+    alive = greedy(number, k, index + 1, answer + number[index])
+    return max(alive,remove)
